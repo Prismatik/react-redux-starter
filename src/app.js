471b6b6cd2c8d5637ceccb9a586875e26a5405c6
Nathan Winch <nwinch@gmail.com>
2016-05-06 09:59:39 +1000

    Use hash history

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import configureStore from './redux/store';
 import routes from './routes';
 
@@ -9,6 +9,6 @@ const store = configureStore();
 
 ReactDOM.render((
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes(store)} />
+    <Router history={hashHistory} routes={routes(store)} />
   </Provider>
 ), document.getElementById('app'));
