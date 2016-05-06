3c98f668ab0eb66f79a34a9d549d09463a82b788
Nathan Winch <nwinch@gmail.com>
2016-05-06 13:49:41 +1000

    Remove comment

--- test/react_utils.js
+++ test/react_utils.js
@@ -1,11 +1,6 @@
-import TestUtils from 'react-addons-test-utils';
-
-// TODO: Hmm having problems with this trying to trigger from mocha.opts with
-// --require ./test/must ... or something like that.
-// Not working so adding here, but it should be in it's own setup file.
-// @nwinch 19/04/16
 import must from 'must';
 import mustJSX from 'must-jsx';
+import TestUtils from 'react-addons-test-utils';
 
 mustJSX(must);
 
